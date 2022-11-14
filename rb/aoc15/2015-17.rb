a97239c45d35a4b8449dff6bee3b9e16e10bfe1b
Robert Fisher <services@id264.net>
2022-11-14 11:19:58 +0000

    2015 day 17

---
+++ rb/aoc15/2015-17.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'set'
+require_relative '../lib/base'
+
+# https://adventofcode.com/2015/day/17
+#
+class Aoc201517
+  def solve01(input, val = 150)
+    input = input.as_ints
+
+    1.upto(input.size).inject(0) do |aggr, l|
+      aggr += input.combination(l).count { |c| c.sum == val }
+    end
+  end
+
+  def solve02(input, val = 150)
+    input = input.as_ints
+
+    combis = 1.upto(input.size).inject([]) do |aggr, l|
+      aggr += input.combination(l).select { |c| c.sum == val }
+    end
+
+    min_num = combis.map(&:size).min
+    combis.count { |c| c.size == min_num }
+  end
+end
+
+class TestAoc201517 < MiniTest::Test
+  include TestBase
+
+  def answer01
+    4
+  end
+
+  def answer02
+    3
+  end
+
+  def sample
+    <<~EOSAMPLE
+      20
+      15
+      10
+      5
+      5
+    EOSAMPLE
+  end
+
+  def sample_val
+    25
+  end
+end
