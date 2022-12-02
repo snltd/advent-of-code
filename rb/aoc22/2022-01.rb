9a4e9e485c4e1299a27c23153094738fd8e5d78b
Robert Fisher <services@id264.net>
2022-12-02 00:35:48 +0000

    it's 2022!

---
+++ rb/aoc22/2022-01.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require_relative '../lib/base'
+
+# https://adventofcode.com/2022/day/01
+#
+class Aoc202201
+  def solve01(input)
+    input.as_num_blocks.map(&:sum).max
+  end
+
+  def solve02(input)
+    input.as_num_blocks.map(&:sum).sort.reverse.take(3).sum
+  end
+end
+
+class TestAoc202201 < MiniTest::Test
+  include TestBase
+
+  def answer01
+    24000
+  end
+
+  def answer02
+    45000
+  end
+
+  def sample
+    <<~EOSAMPLE
+      1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+    EOSAMPLE
+  end
+end
