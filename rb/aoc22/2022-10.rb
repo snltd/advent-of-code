d22522f2b11c6b421f6666d2a66796f10b3b9413
Robert Fisher <services@id264.net>
2022-12-10 18:27:16 +0000

    day 10 in Ruby

---
+++ rb/aoc22/2022-10.rb
@@ -0,0 +1,205 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require_relative '../lib/base'
+
+# https://adventofcode.com/2022/day/10
+#
+class Aoc202210
+  def solve01(input)
+    cycles = input_cycles(input.as_lines)
+    [20, 60, 100, 140, 180, 220].sum { |c| cycles[c - 1] * c }
+  end
+
+  def solve02(input)
+    cycles = input_cycles(input.as_lines)
+
+    0.upto(5).with_object((+"\n")) do |row, ret|
+      0.upto(39) do |c|
+        ret.<< [c - 1, c, c + 1].include?(cycles[c + (row * 40)]) ? '#' : '.'
+      end
+      ret.<< "\n"
+    end
+  end
+
+  private
+
+  def input_cycles(input)
+    input.each.with_object([1]) do |l, ret|
+      ret.<< ret.last
+      ret.<< (ret.last + l.split.last.to_i) if l != 'noop'
+    end
+  end
+end
+
+class TestAoc202210 < MiniTest::Test
+  include TestBase
+
+  def answer01
+    13_140
+  end
+
+  def answer02
+    <<~EOANSWER
+
+      ##..##..##..##..##..##..##..##..##..##..
+      ###...###...###...###...###...###...###.
+      ####....####....####....####....####....
+      #####.....#####.....#####.....#####.....
+      ######......######......######......####
+      #######.......#######.......#######.....
+    EOANSWER
+  end
+
+  def sample
+    <<~EOSAMPLE
+      addx 15
+      addx -11
+      addx 6
+      addx -3
+      addx 5
+      addx -1
+      addx -8
+      addx 13
+      addx 4
+      noop
+      addx -1
+      addx 5
+      addx -1
+      addx 5
+      addx -1
+      addx 5
+      addx -1
+      addx 5
+      addx -1
+      addx -35
+      addx 1
+      addx 24
+      addx -19
+      addx 1
+      addx 16
+      addx -11
+      noop
+      noop
+      addx 21
+      addx -15
+      noop
+      noop
+      addx -3
+      addx 9
+      addx 1
+      addx -3
+      addx 8
+      addx 1
+      addx 5
+      noop
+      noop
+      noop
+      noop
+      noop
+      addx -36
+      noop
+      addx 1
+      addx 7
+      noop
+      noop
+      noop
+      addx 2
+      addx 6
+      noop
+      noop
+      noop
+      noop
+      noop
+      addx 1
+      noop
+      noop
+      addx 7
+      addx 1
+      noop
+      addx -13
+      addx 13
+      addx 7
+      noop
+      addx 1
+      addx -33
+      noop
+      noop
+      noop
+      addx 2
+      noop
+      noop
+      noop
+      addx 8
+      noop
+      addx -1
+      addx 2
+      addx 1
+      noop
+      addx 17
+      addx -9
+      addx 1
+      addx 1
+      addx -3
+      addx 11
+      noop
+      noop
+      addx 1
+      noop
+      addx 1
+      noop
+      noop
+      addx -13
+      addx -19
+      addx 1
+      addx 3
+      addx 26
+      addx -30
+      addx 12
+      addx -1
+      addx 3
+      addx 1
+      noop
+      noop
+      noop
+      addx -9
+      addx 18
+      addx 1
+      addx 2
+      noop
+      noop
+      addx 9
+      noop
+      noop
+      noop
+      addx -1
+      addx 2
+      addx -37
+      addx 1
+      addx 3
+      noop
+      addx 15
+      addx -21
+      addx 22
+      addx -6
+      addx 1
+      noop
+      addx 2
+      addx 1
+      noop
+      addx -10
+      noop
+      noop
+      addx 20
+      addx 1
+      addx 2
+      addx 2
+      addx -6
+      addx -11
+      noop
+      noop
+      noop
+    EOSAMPLE
+  end
+end
