9876e011728d9851ff699ac8bbea434ba2b92733
Robert Fisher <services@id264.net>
2024-12-04 14:22:53 +0000

    code tidy

--- rs/aoc24/src/days/day_04.rs
+++ rs/aoc24/src/days/day_04.rs
@@ -2,20 +2,24 @@ use crate::utils::loader::InputLines;
 
 type Letters = Vec<char>;
 
-pub fn part_01(input: &InputLines) -> i32 {
-    let width: i32 = input[0].len() as i32 + 2;
+fn padded(input: &InputLines) -> (Letters, usize) {
+    let width = input[0].len() + 2;
     let padded_lines: Vec<String> = input.iter().map(|l| format!(".{}.", l)).collect();
     let padded_input: String = padded_lines.join("");
-    let letters: Letters = padded_input.chars().collect();
+    (padded_input.chars().collect(), width)
+}
+
+pub fn part_01(input: &InputLines) -> i32 {
+    let (letters, width) = padded(input);
     let mut found = 0;
 
     for i in 0..=letters.len() {
         for offset in [1, width, width - 1, width + 1] {
-            if offset_search(&letters, i, offset) {
+            if offset_search(&letters, i, offset as isize) {
                 found += 1;
             }
 
-            if offset_search(&letters, i, -offset) {
+            if offset_search(&letters, i, 0 - offset as isize) {
                 found += 1;
             }
         }
@@ -24,24 +28,21 @@ pub fn part_01(input: &InputLines) -> i32 {
     found
 }
 
-fn offset_search(letters: &Letters, index: usize, offset: i32) -> bool {
-    [(0, &'X'), (1, &'M'), (2, &'A'), (3, &'S')]
+fn offset_search(letters: &Letters, index: usize, offset: isize) -> bool {
+    [(0, 'X'), (1, 'M'), (2, 'A'), (3, 'S')]
         .iter()
-        .all(|(m, c)| letter_at(letters, (index as i32 + m * offset) as usize, c))
+        .all(|(m, c)| letter_at(letters, (index as isize + m * offset) as usize, *c))
 }
 
-fn letter_at(letters: &Letters, index: usize, desired: &char) -> bool {
+fn letter_at(letters: &Letters, index: usize, desired: char) -> bool {
     match letters.get(index) {
-        Some(c) => c == desired,
+        Some(c) => c == &desired,
         None => false,
     }
 }
 
 pub fn part_02(input: &InputLines) -> i32 {
-    let width = input[0].len() + 2;
-    let padded_lines: Vec<String> = input.iter().map(|l| format!(".{}.", l)).collect();
-    let padded_input: String = padded_lines.join("");
-    let letters: Letters = padded_input.chars().collect();
+    let (letters, width) = padded(input);
     let mut found: i32 = 0;
 
     for i in 0..=letters.len() {
@@ -52,44 +53,41 @@ pub fn part_02(input: &InputLines) -> i32 {
 }
 
 fn mas_search(letters: &Letters, index: usize, width: usize) -> i32 {
-    let mut matches = 0;
     let arrangements = [
         [
-            (0, &'M'),
-            (2, &'S'),
-            (width + 1, &'A'),
-            (2 * width, &'M'),
-            (2 * width + 2, &'S'),
+            (0, 'M'),
+            (2, 'S'),
+            (width + 1, 'A'),
+            (2 * width, 'M'),
+            (2 * width + 2, 'S'),
         ],
         [
-            (0, &'M'),
-            (2, &'M'),
-            (width + 1, &'A'),
-            (2 * width, &'S'),
-            (2 * width + 2, &'S'),
+            (0, 'M'),
+            (2, 'M'),
+            (width + 1, 'A'),
+            (2 * width, 'S'),
+            (2 * width + 2, 'S'),
         ],
         [
-            (0, &'S'),
-            (2, &'S'),
-            (width + 1, &'A'),
-            (2 * width, &'M'),
-            (2 * width + 2, &'M'),
+            (0, 'S'),
+            (2, 'S'),
+            (width + 1, 'A'),
+            (2 * width, 'M'),
+            (2 * width + 2, 'M'),
         ],
         [
-            (0, &'S'),
-            (2, &'M'),
-            (width + 1, &'A'),
-            (2 * width, &'S'),
-            (2 * width + 2, &'M'),
+            (0, 'S'),
+            (2, 'M'),
+            (width + 1, 'A'),
+            (2 * width, 'S'),
+            (2 * width + 2, 'M'),
         ],
     ];
 
-    for a in arrangements {
-        if a.iter().all(|(m, c)| letter_at(letters, index + m, c)) {
-            matches += 1;
-        }
-    }
-    matches
+    arrangements
+        .iter()
+        .filter(|a| a.iter().all(|(m, c)| letter_at(letters, index + m, *c)))
+        .count() as i32
 }
 
 #[cfg(test)]
