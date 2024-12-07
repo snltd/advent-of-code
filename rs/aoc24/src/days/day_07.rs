5105b648b93e0167e64947416b35666b7cdcdec6
Robert Fisher <services@id264.net>
2024-12-07 14:54:22 +0000

    use rayon

--- rs/aoc24/src/days/day_07.rs
+++ rs/aoc24/src/days/day_07.rs
@@ -1,16 +1,14 @@
 use crate::utils::loader::InputLines;
+use rayon::prelude::*;
 
 pub fn part_01(input: &InputLines) -> usize {
-    let mut ret = 0;
-
-    for row in input {
-        ret += process_line(row);
-    }
-
-    ret
+    input
+        .par_iter()
+        .map(|row| process_line(row, false))
+        .sum::<usize>()
 }
 
-fn process_line(row: &str) -> usize {
+fn process_line(row: &str, use_concat: bool) -> usize {
     let chunks: Vec<&str> = row.split(": ").collect();
     let ck = chunks[0].parse::<usize>().unwrap();
     let nums: Vec<usize> = chunks[1]
@@ -32,6 +30,13 @@ fn process_line(row: &str) -> usize {
 
             if n_plus <= ck {
                 new_vec.push(n_plus);
+            }
+
+            if use_concat {
+                let n_concat = format!("{s}{val}").parse::<usize>().unwrap();
+                if n_concat <= ck {
+                    new_vec.push(n_concat);
+                }
             }
         }
 
@@ -46,53 +51,10 @@ fn process_line(row: &str) -> usize {
 }
 
 pub fn part_02(input: &InputLines) -> usize {
-    let mut ret = 0;
-
-    for row in input {
-        ret += process_line_02(row);
-    }
-
-    ret
-}
-
-fn process_line_02(row: &str) -> usize {
-    let chunks: Vec<&str> = row.split(": ").collect();
-    let ck = chunks[0].parse::<usize>().unwrap();
-    let nums: Vec<usize> = chunks[1]
-        .split_whitespace()
-        .map(|n| n.parse::<usize>().unwrap())
-        .collect();
-
-    let mut start_vec: Vec<usize> = vec![nums[0]];
-
-    for val in nums[1..].iter() {
-        let mut new_vec: Vec<usize> = Vec::new();
-        for s in start_vec.iter() {
-            let n_mult = s * val;
-            let n_plus = s + val;
-            let n_concat = format!("{s}{val}").parse::<usize>().unwrap();
-
-            if n_mult <= ck {
-                new_vec.push(n_mult);
-            }
-
-            if n_plus <= ck {
-                new_vec.push(n_plus);
-            }
-
-            if n_concat <= ck {
-                new_vec.push(n_concat);
-            }
-        }
-
-        start_vec = new_vec;
-    }
-
-    if start_vec.contains(&ck) {
-        ck
-    } else {
-        0
-    }
+    input
+        .par_iter()
+        .map(|row| process_line(row, true))
+        .sum::<usize>()
 }
 
 #[cfg(test)]
